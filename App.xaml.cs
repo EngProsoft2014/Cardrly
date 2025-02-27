f871afaab34bcfb4adec59374950a36ac69eba12
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2025-02-27 21:00:22 +0200

    update Expire date

--- App.xaml.cs
+++ App.xaml.cs
@@ -66,7 +66,7 @@ namespace Cardrly
                 }
                 else
                 {
-                    MainPage = new NavigationPage(new HomePage(new HomeViewModel(Rep, _service, audioManager), Rep, _service));
+                    MainPage = new NavigationPage(new LoginPage(new LoginViewModel(Rep, _service, audioManager)));
                 }
                 Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
             }
