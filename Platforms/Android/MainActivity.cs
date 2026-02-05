b98c51cf6998102f168aa1aa8c6164ba65115553
AhmedFayez299 <93268385+AhmedFayez299@users.noreply.github.com>
2026-02-05 13:03:00 +0200

    Add FirebaseCloudMessaging

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -9,10 +9,9 @@ using Android.Views;
 using AndroidX.Core.App;
 using AndroidX.Core.Content;
 using Plugin.NFC;
-//using Plugin.Firebase.CloudMessaging;
-//using Firebase;
 using Cardrly.Services;
 using Cardrly.Platforms.Android;
+using Plugin.Firebase.CloudMessaging;
 namespace Cardrly
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Portrait)]
@@ -97,7 +96,7 @@ namespace Cardrly
 
         private static void HandleIntent(Intent intent)
         {
-            //FirebaseCloudMessagingImplementation.OnNewIntent(intent);
+            FirebaseCloudMessagingImplementation.OnNewIntent(intent);
         }
 
         private void CreateNotificationChannelIfNeeded()
@@ -114,7 +113,7 @@ namespace Cardrly
             var notificationManager = (NotificationManager)GetSystemService(NotificationService);
             var channel = new NotificationChannel(channelId, "General", NotificationImportance.Default);
             notificationManager.CreateNotificationChannel(channel);
-            //FirebaseCloudMessagingImplementation.ChannelId = channelId;
+            FirebaseCloudMessagingImplementation.ChannelId = channelId;
         }
 
         static void CreateNotificationFromIntent(Intent intent)
